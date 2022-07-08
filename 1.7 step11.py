395960f5babd34a47fad2a1920ef3cf73980b4db
BuildTools <unconfigured@null.spigotmc.org>
2022-07-08 23:13:21 +0500

    start

---
+++ 1.7 step11.py
@@ -0,0 +1,42 @@
+class AppStore:
+    def __init__(self):
+        blocked = False
+
+    def add_application(self, app):
+        pass
+
+    def remove_application(self, app):
+        pass
+
+    def block_application(self, app):
+        pass
+
+    def total_apps(self):
+        pass
+
+
+class Application:
+    def __init__(self, name, blocked=False):
+        self.name = name
+        self.blocked = blocked
+
+
+# 'add_application(self, app) - добавление нового приложения app в магазин;
+# remove_application(self, app) - удаление приложения app из магазина;
+# block_application(self, app) - блокировка приложения app (устанавливает локальное свойство blocked объекта app в значение True);
+# total_apps(self) - возвращает общее число приложений в магазине.
+#
+# Класс AppStore предполагается использовать следующим образом (эти строчки в программе не писать):
+#
+store = AppStore()
+app_youtube = Application("Youtube")
+store.add_application(app_youtube)
+store.remove_application(app_youtube)
+# Здесь Application - класс, описывающий добавляемое приложение с указанным именем. Каждый объект класса Application должен содержать локальные свойства:
+#
+# name - наименование приложения (строка);
+# blocked - булево значение (True - приложение заблокировано; False - не заблокировано, изначально False).
+#
+# Как хранить список приложений в объектах класса AppStore решите сами.
+#
+# P.S. В программе нужно только объявить классы с указанным функционалом.'
